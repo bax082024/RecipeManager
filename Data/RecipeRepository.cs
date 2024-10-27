edfa2be43b255a3a288d329a01690c44f8633023
bax082024 <bax082024@gmail.com>
2024-10-27 06:39:50 +0100

    save list to json added

--- Data/RecipeRepository.cs
+++ Data/RecipeRepository.cs
@@ -8,7 +8,13 @@ namespace RecipeManager.Data
 {
   public class RecipeRepository
   {
-    
+    private readonly string filePath = "Data/recipes.json";
+
+    public void SaveRecipes(List<Recipe> recipes)
+    {
+      string json = JsonConvert.SerializeObject(recipes, Formatting.Indented);
+      File.WriteAllText(filePath, json);
+    }
   }
 
 }
